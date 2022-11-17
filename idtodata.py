a712abdcd551822d12b9a3b5095ed9a0f46b4ec0
gkizildogan <goksu.kizildogan@gmail.com>
2022-11-17 23:50:03 +0300

    Able to scrape a given page into dataframe/csv

--- idtodata.py
+++ idtodata.py
@@ -19,8 +19,20 @@ chrome_options.add_argument("--incognito")
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://www.sahibinden.com/")
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//input[@id='searchText']"))).send_keys(ad_id)
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@value='Ara']"))).click()
+
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='searchText']"))).send_keys(ad_id)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@value='Ara']"))).click()
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, features="lxml")
-driver.close()
+ilan_bilgileri = driver.find_elements(By.XPATH, "//ul[@class='classifiedInfoList']/li/strong")
+ilan_degerleri = driver.find_elements(By.XPATH, "//ul[@class='classifiedInfoList']/li/span")
+fiyat_satiri = driver.find_element(By.XPATH, "//div[@class = 'classifiedInfo ']/h3")
+fiyat_metni = fiyat_satiri.text
+fiyat = fiyat_metni.split('\n', 1)[0]
+bilgiler = [bilgi.text for bilgi in ilan_bilgileri]
+bilgiler.append("İlan Fiyatı")
+degerler = [deger.text for deger in ilan_degerleri]
+degerler.append(fiyat)
+ilan = pd.DataFrame(data=np.array(degerler).reshape(1, -1), columns=bilgiler)
+ilan.to_csv("ilanlar.csv", mode="a", index=False, header=True)
+driver.quit()
