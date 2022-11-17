17c78a830a32e9fd06ce1aa447f6161c87693cf5
gkizildogan <goksu.kizildogan@gmail.com>
2022-11-17 23:57:11 +0300

    Able to scrape a given page into dataframe/csv

--- idtodata.py
+++ idtodata.py
@@ -22,8 +22,7 @@ driver.get("https://www.sahibinden.com/")
 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='searchText']"))).send_keys(ad_id)
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@value='Ara']"))).click()
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, features="lxml")
+
 ilan_bilgileri = driver.find_elements(By.XPATH, "//ul[@class='classifiedInfoList']/li/strong")
 ilan_degerleri = driver.find_elements(By.XPATH, "//ul[@class='classifiedInfoList']/li/span")
 fiyat_satiri = driver.find_element(By.XPATH, "//div[@class = 'classifiedInfo ']/h3")
